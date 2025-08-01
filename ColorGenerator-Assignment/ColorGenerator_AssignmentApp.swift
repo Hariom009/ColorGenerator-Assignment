726c19e88cd2bf809ddc29ab6cce8dbcb1ce69fe
Hariom <hariomconnects03@gmail.com>
2025-08-01 11:48:49 +0530

    Initial Commit

---
+++ ColorGenerator-Assignment/ColorGenerator_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGenerator_AssignmentApp.swift
+//  ColorGenerator-Assignment
+//
+//  Created by Hari's Mac on 01.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorGenerator_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
